07ad40689ba8e3b0191c03d775d408e2f1ffd827
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-23 17:00:47 +0330

    connectDB & .env created

--- project/pages/api/data/index.js
+++ project/pages/api/data/index.js
@@ -1,10 +1,20 @@
-export default function handler(req, res) {
+import mongoose from "mongoose";
+import connectDB from './../../../utils/connectDB';
+
+export default async function handler(req, res) {
+    await connectDB();
   if (req.method === "POST") {
     const { name } = req.body;
     if (!name || name.length <= 3) {
       res.status(422).json({ status: "failed", message: "Invalid Data" });
       return;
     }
+
+    //connect to DB
+    // mongoose.connect(
+    //   "mongodb+srv://jamal:j11067@cluster0.lnfefkh.mongodb.net/?retryWrites=true&w=majority", 
+    //   () => console.log("Connect to DB")
+    // );
     res
       .status(201)
       .json({ status: "Sucess", message: "Data Created", data: { name } });
