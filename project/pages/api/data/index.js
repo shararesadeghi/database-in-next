7e81a973f988f5958d5816d9d56661b477924380
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-27 16:23:10 +0330

    handle some erros & create model

--- project/pages/api/data/index.js
+++ project/pages/api/data/index.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
-import connectDB from './../../../utils/connectDB';
+import connectDB from "./../../../utils/connectDB";
+import User from "./../../../models/User";
 
 export default async function handler(req, res) {
+  try {
     await connectDB();
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ status: "failed", message: "Error in connecting to database" });
+  }
   if (req.method === "POST") {
     const { name } = req.body;
     if (!name || name.length <= 3) {
@@ -12,11 +20,23 @@ export default async function handler(req, res) {
 
     //connect to DB
     // mongoose.connect(
-    //   "mongodb+srv://jamal:j11067@cluster0.lnfefkh.mongodb.net/?retryWrites=true&w=majority", 
+    //   "mongodb+srv://jamal:j11067@cluster0.lnfefkh.mongodb.net/?retryWrites=true&w=majority",
     //   () => console.log("Connect to DB")
     // );
-    res
-      .status(201)
-      .json({ status: "Sucess", message: "Data Created", data: { name } });
+
+    // const user = new User({name});
+    // await user.save();
+
+    try {
+      const user = await User.create({ name });
+      res
+        .status(201)
+        .json({ status: "sucess", message: "Data Created", data: user });
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ status: "failed", message: "Error in connecting to database" });
+    }
   }
 }
--- project/utils/connectDB.js
+++ project/utils/connectDB.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 async function connectDB(){
-    try{
+    // try{
         if(mongoose.connections[0].readyState) return;
 
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Connect to DB");
 
-    }catch(err){
-        console.log("Connection failed")
-    }
+    // }catch(err){
+        //  console.log("Connection failed")
+    // }
 }
 
 export default connectDB;
